0da66e7bb9e3a26302bf68cbe851ae5a34cabc21
leducanh120197 <leducanh12011997@gmail.com>
2019-02-25 14:11:50 +0700

    Sign Up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get "user/new"
+  get "user/show"
   root "static_pages#show", page: "home"
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users
   resources :stories
 end
