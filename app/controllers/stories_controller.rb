8cfad78e1899d4af9398bc5f635a704a84b4892c
leducanh120197 <leducanh12011997@gmail.com>
2019-02-27 14:29:16 +0700

    Show a story

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -3,4 +3,10 @@ class StoriesController < ApplicationController
     @stories = Story.select(:id, :name, :author, :description, :process,
       :status).page(params[:page]).per Settings.stories.number_stories
   end
+
+  def show
+    @story = Story.find_by id: params[:id]
+    @stories = Story.select(:id, :name, :author).page(params[:page])
+                    .per Settings.stories.number_hot_stories
+  end
 end
